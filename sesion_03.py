9eaa41db31ad1374ccd9993be93850be5b8b9b21
Margaritahrdz <margaritahrdzgl@gmail.com>
2026-02-13 15:31:17 -0800

    se añadio el documentos de los dias

---
+++ sesion_03.py
@@ -0,0 +1,32 @@
+# loops
+mi_lista = [1,2,3,4,5]
+
+for i in mi_lista:
+    print("El numero es", i)
+    
+resultado = 0
+for i in mi_lista:
+    resultado +=1
+
+print(f"el resultado de la suma de mi lista es: {resultado}")
+
+for i in range (2,9):
+    print(i)
+
+mi_lista_2 = ["Lunes, Martes; Miercoles, Jueves, Viernes"]
+for i in mi_lista_2:
+    if i != "lunes":
+     print(f"feliz {i}!")
+
+#While loop
+i = 0
+
+while i < 5:
+   i += 1
+   if i ==3:
+      continue
+   print(i)
+   if i == 4:
+      break
+else:
+   print("i es ahora mayor o igual a 5")
